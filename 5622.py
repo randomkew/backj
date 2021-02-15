86f5b859bbb624c8a43193760a6d227e0da4089f
randomkew <39371175+randomkew@users.noreply.github.com>
2021-02-15 21:54:12 +0900

    02-15

--- 5622.py
+++ 5622.py
@@ -1,21 +1,12 @@
-a=list(input().lower())
-sum=0
-print(a)
-for i in range(len(a)):
-    if(ord(a[i])>=97 and ord(a[i])<100):#2
-        sum+=3
-    elif(ord(a[i])>=100 and ord(a[i])<103):#3
-        sum+=4
-    elif(ord(a[i])>=103 and ord(a[i])<106):#4
-        sum+=5
-    elif(ord(a[i])>=106 and ord(a[i])<109):#5
-        sum+=6
-    elif(ord(a[i])>=109 and ord(a[i])<112):#6
-        sum+=7
-    elif(ord(a[i])>=112 and ord(a[i])<116):#7
-        sum+=8
-    elif(ord(a[i])>=116 and ord(a[i])<119):#8
-        sum+=9
-    elif(ord(a[i])>=119 and ord(a[i])<123):#9
-        sum+=10
-    print(sum)
+t = int(input())
+
+for _ in range(t):
+    floor = int(input())
+    num = int(input())
+    f0 = [x for x in range(1, num+1)]
+    for k in range(floor):
+        for i in range(1, num):
+            f0[i] += f0[i-1]
+            print(f0[i-1])
+        print(f0)  # 프린트문을 추가
+    print(f0[-1])
